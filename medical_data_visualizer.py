2e38b4217777a39fa5d11100f16b1df609617f07
Carlos Melo, PhD <carlosmelo@hotmail.com.br>
2024-09-17 19:36:08 +0000

    done

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,57 +4,59 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+df['overweight'] = ((df['weight']/((df['height']/100)**2)) > 25).astype(int)
 
 # 3
-
+df.replace(
+    {"cholesterol": {1: 0, 2: 1, 3: 1}, "gluc": {1: 0, 2: 1, 3: 1}},
+    inplace=True
+)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # Convertendo o DataFrame para o formato longo
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
+    # Criando o gráfico
+    fig = sns.catplot(data=df_cat, kind='count', x='variable', hue='value', col='cardio')
 
-    # 6
-    df_cat = None
-    
+    # Ajustando o rótulo do eixo y
+    for ax in fig.axes.flat:
+        ax.set_ylabel('total')
 
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    # Salvando o gráfico
     fig.savefig('catplot.png')
-    return fig
+    return fig.fig
 
 
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    q1 = (df['ap_lo'] <= df['ap_hi'])
+    q2 = (df['height'] >= df['height'].quantile(0.025))
+    q3 = (df['height'] <= df['height'].quantile(0.975))
+    q4 = (df['weight'] >= df['weight'].quantile(0.025))
+    q5 = (df['weight'] <= df['weight'].quantile(0.975))
+
+    df_heat = df.loc[(q1 & q2 & q3 & q4 & q5)]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
+    plt.figure(figsize=(10, 8))
+    ax = sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', linewidths=0.5)
 
     # 15
 
 
 
     # 16
-    fig.savefig('heatmap.png')
-    return fig
+    plt.savefig('heatmap.png')
+    return plt.gcf()
